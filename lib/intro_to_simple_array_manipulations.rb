4754166b06b6e9caa5c2bd85968f1923aa6431f7
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-05 19:06:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -43,6 +43,6 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, integer)
-  array.delete(integer)
+def using_delete_at(array, integer(index))
+  array.delete(index, integer)
 end
